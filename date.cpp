7c6c57b5630d64758ce606ce21be7234c1a77392
Andreas Stallinger <astallinger@gmail.com>
2013-02-28 23:50:05 +0100

    Update date.cpp
    replaced switch with if

--- date.cpp
+++ date.cpp
@@ -46,24 +46,15 @@ bool Date::operator> (const Date& other){
 }
 
 int Date::monthDays(int month, int year)const{
-    switch(month){
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            return 31;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return 30;
-        case 2:
-            if(isLeapYear(year)) return 29;
-            return 28;
-    }
+    if( month == 1 || month == 3 || month == 5 || month == 7 || 
+        month == 8 || month == 10 || month == 12)
+        return 31;
+    if(month == 4 || month == 6 || month == 9 || month == 11)
+        return 30;
+    if(isLeapYear(year)) 
+        return 29;
+    return 28;
+
 }
 
 int Date::dayOfYear()const{
